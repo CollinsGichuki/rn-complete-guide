9405d8648bdcef0f656e780653af4114b0c5bb17
Snilloc <collinsgichuki9@gmail.com>
2021-02-03 10:03:01 +0300

    Added User Input

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+
+function CounterButton({ title, onIncrement}) {
+  return <Button title={title} onPress={onIncrement} />
+}
+
+//Function Components take parameters(props) and returns a React Element
 export default function App() {
+  const add = (myNumber) => (myNumber + 5);
+
+  const subtract = (myOtherNumber, myOtherOtherNumber) => {
+    if (myOtherNumber > myOtherOtherNumber) {
+      return myOtherNumber - myOtherOtherNumber;
+    } else {
+      return myOtherOtherNumber - myOtherNumber;
+    }
+  }
+
+  const [count, setCount] = useState(0)
+
+  const [text, setText] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> { [10, 56, 98].map(add)}</Text>
+      <Text style={{fontSize : 120 }}>{ subtract(23, 34)}</Text>
+      <CounterButton 
+      title={`Click the button to increase the count: ${count}`} 
+      onIncrement = {() => setCount(count + 1)}/>
+      <TextInput 
+      value={text}
+      style={{ fontSize: 32, color: 'steelblue'}}
+      placeholder="Type here.."
+      onChangeText={ (text) => {
+        setText (text)
+      }}
+      />
+      <Text style={{ fontSize: 21}}>{'\n'} You entered; {text}</Text>
       <StatusBar style="auto" />
     </View>
   );
